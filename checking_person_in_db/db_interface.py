ec3ac7ee5a423de55dc65bfaf36158684828b770
OlgaKochneva <kochnevaolga74@bk.ru>
2019-12-19 14:51:43 +0300

    #50 Some cosmetic fixes

--- checking_person_in_db/db_interface.py
+++ checking_person_in_db/db_interface.py
@@ -1,25 +1,26 @@
 import _pickle
 import os
-import face_recognition
 import re
+
+import face_recognition
 from app.model import Persons
 
 
-def init_database(path_to_train_dir='../resources/train/'):  # change path
+def insert_many_persons(path_to_train_dir):  # change path
     train_dir = os.listdir(path_to_train_dir)
     count_persons = len(train_dir)
     person_counter = 0
     train_dir = os.listdir(path_to_train_dir)
-    print(f'\r0/{count_persons} of people have been processed (0.0%).', end='')
+    print(f'{count_persons} of people have been processed (0.0%)', end='')
 
     for path_to_person_dir in train_dir:
-        insert_person(path_to_person_dir, path_to_train_dir + path_to_person_dir)
+        insert_one_person(path_to_person_dir, path_to_train_dir + path_to_person_dir)
         person_counter += 1
         print(f'{person_counter}/{count_persons} '
-              f'of people have been processed ({person_counter / count_persons * 100: .0f}%).', end='')
+              f'of people have been processed ({person_counter / count_persons * 100: .0f}%) ', end='')
 
 
-def insert_person(name, path_to_person_dir):
+def insert_one_person(name, path_to_person_dir):
     try:
         paths_to_person_imgs = os.listdir(path_to_person_dir)
     except FileNotFoundError:
@@ -28,6 +29,9 @@ def insert_person(name, path_to_person_dir):
 
     persons = Persons.objects(name=name)
     encodings = _pickle.loads(persons[0].face_encodings) if persons else []
+
+# TODO Add regex expression for jpg, png etc files for this line
+#   face = face_recognition.load_image_file(path_to_person_dir + '/' + path_to_person_img)
 
     for path_to_person_img in paths_to_person_imgs:
         face = face_recognition.load_image_file(
@@ -43,14 +47,14 @@ def insert_person(name, path_to_person_dir):
     print(f'{name} successfully added')
 
 
-def delete_person(name='.*'):
-    regex = re.compile(name)
+def delete_person(name=''):
+    regex = re.compile('.*' + name)
     person = Persons.objects(name=regex)
     if person:
         person.delete()
-        print(f'All {name}\'s data was deleted')
+        print(f'All {name} data was deleted')
     else:
-        print('No such person in bd')
+        print('No such person in db')
 
 
 def show_persons():
@@ -60,15 +64,14 @@ def show_persons():
             print(name)
     else:
         print("No persons in db")
-    return names
 
 
 if __name__ == '__main__':
     # delete_person()
     show_persons()
-    # insert_person('Morgan_Freeman', '../resources/train/Morgan_Freeman')
+    # insert_one_person('Morgan_Freeman', '../resources/train/Morgan_Freeman')
     # show_persons()
     # delete_person("Morgan_Freeman")
     # show_persons()
-    # init_database('../resources/train/')
+    # insert_many_persons('../resources/train/')
     # show_persons()
